2afe3d8d9ca301498f3557186be2e735fe2ff293
lightx <lightx@outlook.com>
2026-06-03 11:25:56 +0800

    perf: require explicit cache-line atomic orders

--- include/af/detail/runtime/runtime_common_state.hpp
+++ include/af/detail/runtime/runtime_common_state.hpp
@@ -23,11 +23,11 @@ template <typename T> struct alignas(hardware_cache_line_size) CacheLineAtomic {
     CacheLineAtomic(const CacheLineAtomic &) = delete;
     CacheLineAtomic &operator=(const CacheLineAtomic &) = delete;
 
-    [[nodiscard]] T load(std::memory_order order = std::memory_order_seq_cst) const noexcept {
+    [[nodiscard]] T load(std::memory_order order) const noexcept {
         return value.load(order);
     }
 
-    void store(T desired, std::memory_order order = std::memory_order_seq_cst) noexcept {
+    void store(T desired, std::memory_order order) noexcept {
         value.store(desired, order);
     }
 
@@ -41,19 +41,19 @@ template <typename T> struct alignas(hardware_cache_line_size) CacheLineAtomic {
         return value.compare_exchange_strong(expected, desired, success, failure);
     }
 
-    T fetch_add(T arg, std::memory_order order = std::memory_order_seq_cst) noexcept {
+    T fetch_add(T arg, std::memory_order order) noexcept {
         return value.fetch_add(arg, order);
     }
 
-    T fetch_sub(T arg, std::memory_order order = std::memory_order_seq_cst) noexcept {
+    T fetch_sub(T arg, std::memory_order order) noexcept {
         return value.fetch_sub(arg, order);
     }
 
-    T fetch_and(T arg, std::memory_order order = std::memory_order_seq_cst) noexcept {
+    T fetch_and(T arg, std::memory_order order) noexcept {
         return value.fetch_and(arg, order);
     }
 
-    void wait(T old, std::memory_order order = std::memory_order_seq_cst) const noexcept {
+    void wait(T old, std::memory_order order) const noexcept {
         value.wait(old, order);
     }
 
