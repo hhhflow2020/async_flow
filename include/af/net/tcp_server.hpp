ceec3fe5028243d6f78be0db2c8685a5effe75e8
lightx <lightx@outlook.com>
2026-06-05 13:21:27 +0800

    refactor: split tcp handler adapter

--- include/af/net/tcp_server.hpp
+++ include/af/net/tcp_server.hpp
@@ -19,6 +19,7 @@
 #include "af/async_runtime.hpp"
 #include "af/buffer/buffer.hpp"
 #include "af/detail/config.hpp"
+#include "af/net/detail/tcp_handler.hpp"
 #include "af/detail/net/reactor/net_io_channel.hpp"
 #include "af/detail/net/socket_address.hpp"
 #include "af/net/tcp_endpoint.hpp"
@@ -56,122 +57,6 @@ enum class TcpConnectionCommandKind : std::uint8_t {
     ResumeRead,
     SetNoDelay,
     SetKeepAlive,
-};
-
-template <typename Handler, typename Runtime, typename = void>
-struct TcpHandlerHasOnAccept : std::false_type {};
-template <typename Handler, typename Runtime>
-struct TcpHandlerHasOnAccept<Handler, Runtime,
-                             std::void_t<decltype(std::declval<Handler &>().on_accept(
-                                 std::declval<TcpConnectionRef<Runtime>>()))>> : std::true_type {};
-
-template <typename Handler, typename Runtime, typename = void>
-struct TcpHandlerHasOnRead : std::false_type {};
-template <typename Handler, typename Runtime>
-struct TcpHandlerHasOnRead<
-    Handler, Runtime,
-    std::void_t<decltype(std::declval<Handler &>().on_read(
-        std::declval<TcpConnectionRef<Runtime>>(), std::declval<af::BufferView>()))>>
-    : std::true_type {};
-
-template <typename Handler, typename Runtime, typename = void>
-struct TcpHandlerHasOnClose : std::false_type {};
-template <typename Handler, typename Runtime>
-struct TcpHandlerHasOnClose<
-    Handler, Runtime,
-    std::void_t<decltype(std::declval<Handler &>().on_close(
-        std::declval<TcpConnectionHandle<Runtime>>(), std::declval<CloseReason>()))>>
-    : std::true_type {};
-
-template <typename Handler, typename = void>
-struct TcpHandlerHasOnListenerErrorAlias : std::false_type {};
-template <typename Handler>
-struct TcpHandlerHasOnListenerErrorAlias<
-    Handler, std::void_t<decltype(std::declval<Handler &>().on_error(
-                 std::declval<TcpListenerHandle>(), std::declval<int>()))>> : std::true_type {};
-
-template <typename Handler, typename = void>
-struct TcpHandlerHasOnListenerError : std::false_type {};
-template <typename Handler>
-struct TcpHandlerHasOnListenerError<
-    Handler, std::void_t<decltype(std::declval<Handler &>().on_listener_error(
-                 std::declval<TcpListenerHandle>(), std::declval<int>()))>> : std::true_type {};
-
-template <typename Runtime> class TcpHandlerBase {
-public:
-    virtual ~TcpHandlerBase() = default;
-    [[nodiscard]] virtual std::unique_ptr<TcpHandlerBase> clone() const = 0;
-    virtual void on_accept(TcpConnectionRef<Runtime> conn) noexcept = 0;
-    virtual void on_read(TcpConnectionRef<Runtime> conn, af::BufferView bytes) noexcept = 0;
-    virtual void on_close(TcpConnectionHandle<Runtime> conn, CloseReason reason) noexcept = 0;
-    virtual void on_listener_error(TcpListenerHandle listener, int error) noexcept = 0;
-};
-
-template <typename Runtime, typename Handler>
-class TcpHandlerModel final : public TcpHandlerBase<Runtime> {
-public:
-    explicit TcpHandlerModel(Handler handler) : handler_(std::move(handler)) {}
-
-    [[nodiscard]] std::unique_ptr<TcpHandlerBase<Runtime>> clone() const override {
-        return std::make_unique<TcpHandlerModel>(handler_);
-    }
-
-    void on_accept(TcpConnectionRef<Runtime> conn) noexcept override {
-        if constexpr (TcpHandlerHasOnAccept<Handler, Runtime>::value) {
-            try {
-                handler_.on_accept(conn);
-            } catch (...) {
-                conn.close(CloseReason::Error);
-            }
-        } else {
-            static_cast<void>(conn);
-        }
-    }
-
-    void on_read(TcpConnectionRef<Runtime> conn, af::BufferView bytes) noexcept override {
-        if constexpr (TcpHandlerHasOnRead<Handler, Runtime>::value) {
-            try {
-                handler_.on_read(conn, bytes);
-            } catch (...) {
-                conn.close(CloseReason::Error);
-            }
-        } else {
-            static_cast<void>(conn);
-            static_cast<void>(bytes);
-        }
-    }
-
-    void on_close(TcpConnectionHandle<Runtime> conn, CloseReason reason) noexcept override {
-        if constexpr (TcpHandlerHasOnClose<Handler, Runtime>::value) {
-            try {
-                handler_.on_close(conn, reason);
-            } catch (...) {
-            }
-        } else {
-            static_cast<void>(conn);
-            static_cast<void>(reason);
-        }
-    }
-
-    void on_listener_error(TcpListenerHandle listener, int error) noexcept override {
-        if constexpr (TcpHandlerHasOnListenerErrorAlias<Handler>::value) {
-            try {
-                handler_.on_error(listener, error);
-            } catch (...) {
-            }
-        } else if constexpr (TcpHandlerHasOnListenerError<Handler>::value) {
-            try {
-                handler_.on_listener_error(listener, error);
-            } catch (...) {
-            }
-        } else {
-            static_cast<void>(listener);
-            static_cast<void>(error);
-        }
-    }
-
-private:
-    Handler handler_;
 };
 
 template <typename Runtime> struct TcpListenerContext {
